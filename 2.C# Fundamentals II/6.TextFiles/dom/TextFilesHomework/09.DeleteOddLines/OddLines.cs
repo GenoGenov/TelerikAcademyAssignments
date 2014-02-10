fe47012d4489d20884b6faf567a619b04411afc3
Dirty <g.genov1993@gmail.com>
2014-02-10 06:53:59 +0200

    C# PartII

---
+++ 2.C# Fundamentals II/6.TextFiles/dom/TextFilesHomework/09.DeleteOddLines/OddLines.cs
@@ -0,0 +1,59 @@
+﻿//Write a program that deletes from given text file all odd lines. The result should be in the same file.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.DeleteOddLines
+{
+    class OddLines
+    {
+        static void Main(string[] args)
+        {
+            //Path to content folder
+
+            //Tested and working on 220MB txt file.
+            string content = "..\\..\\Content";
+            try
+            {
+                StreamReader rdr = new StreamReader(content + "\\first.txt");
+                StreamWriter wrt = new StreamWriter(content + "\\temp.txt", false, Encoding.GetEncoding("utf-8"));
+                Console.WriteLine("Working..");
+                string line = rdr.ReadLine();
+                int i = 1;
+                while (line != null)
+                {
+
+                    if (i % 2 == 0)
+                    {
+                        wrt.WriteLine(line);
+                        wrt.Flush();
+                    }
+                    line = rdr.ReadLine();
+                    i++;
+                }
+                wrt.Close();
+                rdr.Close();
+                string name = Path.GetFullPath(content + "\\first.txt");
+                File.Delete(content + "\\first.txt");
+                File.Move(content + "\\temp.txt", name);
+
+
+                Console.WriteLine("Success! go check the new file :)");
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ 2.C# Fundamentals II/6.TextFiles/dom/TextFilesHomework/10.ExtractTextFromXML/ExtractXML.cs
@@ -0,0 +1,56 @@
+﻿//Write a program that extracts from given XML file all the text without the tags. 
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace _10.ExtractTextFromXML
+{
+    class ExtractXML
+    {
+        private static string GetTextOnly(XmlNode node)
+        {
+            if (node == null)
+            {
+                return String.Empty;
+            }
+
+            if (!node.HasChildNodes)
+            {
+                return node.InnerText;
+            }
+            else
+            {
+                StringBuilder b = new StringBuilder();
+
+                foreach (XmlNode n in node.ChildNodes)
+                {
+                    string text = GetTextOnly(n);
+                    b.Append(text+"\n");
+                }
+
+                return b.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string content = "..\\..\\Content";
+            XmlDocument xml = new XmlDocument(); ;
+            xml.Load(content + "\\strings.xml");
+
+            string text = GetTextOnly(xml.DocumentElement);
+
+            StreamWriter wrt = new StreamWriter(content + "\\final.txt");
+
+            wrt.WriteLine(text);
+            wrt.Close();
+
+
+        }
+    }
+}
