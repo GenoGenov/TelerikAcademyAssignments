3fcf53995582a3556a225bbef2a8f0f0f4203da2
Geno Genov <g.genov1993@gmail.com>
2014-07-05 23:46:53 +0300

    js oop

---
+++ 9.JavaScript - OOP/7.AMD&RequireJS/dom/RequireJS-Homework/RequireJS-Homework/Scripts/app.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    require.config({
+        paths: {
+            'jquery': 'libs/jquery-2.1.1',
+            'handlebars': 'libs/handlebars'
+        }
+
+    });
+
+    require(['jquery', 'combo-box'], function($, controls) {
+        var people = [
+            { id: 1, name: "Doncho Minkov", age: 18, avatarUrl: "images/minkov.jpg" },
+            { id: 2, name: "Georgi Georgiev", age: 19, avatarUrl: "images/joreto.jpg" }
+        ];
+        var comboBox = controls.ComboBox(people);
+        var template = $("#person-template").html();
+        comboBox.compile(template);
+        comboBox.render('#person-template');
+
+    });
+}());
---
+++ 9.JavaScript - OOP/7.AMD&RequireJS/dom/RequireJS-Homework/RequireJS-Homework/Scripts/combo-box.js
@@ -0,0 +1,52 @@
+﻿var controls = controls || {};
+define(['jquery', 'handlebars'], function($) {
+    controls.ComboBox = (function(items) {
+        var compiledHtml;
+
+        (function($) {
+            var selecting = false;
+            $.fn.ComboBox = function() {
+                var $this = $(this);
+
+                if (!$this.find('.current').length) {
+                    $this.find('.person-item').first().addClass('current');
+                }
+                var currentHolder = $('<div/>').attr('id', 'current-shown').prependTo($this);
+                var personsList = $this.find('#persons-list');
+                personsList.find('.current').appendTo(currentHolder);
+                personsList.find('.person-item').hide();
+                currentHolder.on('click', 'div', function() {
+                    personsList.find('.person-item').show();
+                    selecting = true;
+                });
+                personsList.on('click', 'div', function() {
+                    if (selecting) {
+                        currentHolder.find('.current').removeClass('current').appendTo(personsList);
+                        var $that = $(this);
+                        $that.addClass('current').appendTo(currentHolder);
+                        personsList.find('.person-item').hide();
+                        selecting = false;
+                    }
+                });
+                return $this;
+            };
+        }($));
+
+        var compile = function(template) {
+            var handlebarsTemplate = Handlebars.compile(template);
+            var html = handlebarsTemplate({ items: items });
+            compiledHtml = html;
+            return html;
+        };
+        return {
+            compile: compile,
+            render: function(selector) {
+                $(selector).remove();
+                $('<div/>').attr('id', 'persons-list-combo')
+                    .append($('<div/>').attr('id', 'persons-list').html(compiledHtml))
+                    .ComboBox().appendTo('body');
+            }
+        };
+    });
+    return controls;
+});
