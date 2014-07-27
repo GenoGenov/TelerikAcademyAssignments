b3c8d6f76d925d6c2160fd744f586bf513b0833f
Geno Genov <g.genov1993@gmail.com>
2014-07-27 07:29:53 +0300

    JS Applications Homeworks

---
+++ 10.JavaScript - Applications/3.UnderscoreJS/dom/UnderscoreJS-Homework/UnderscoreJS-Homework/Scripts/animal.js
@@ -0,0 +1,55 @@
+﻿define(function () {
+    var Animal = (function () {
+
+        Animal.SPECIES_TYPES = ['mammal', 'insect', 'reptile', 'fish'];
+
+        function validateLegs(number) {
+            if ((typeof number !== 'number') || number < 0) {
+                throw new Error('Animal legs count cannot be less than 0');
+            }
+        }
+
+        function validateSpecies(name) {
+            if ((typeof name !== 'string') || Animal.SPECIES_TYPES.indexOf(name)<0) {
+                throw new Error('The animal species type is invalid !');
+            }
+        }
+
+        function species(name) {
+            if (name) {
+                validateSpecies(name);
+                this._species = name;
+            } else {
+                return this._species;
+            }
+        }
+
+
+        function numberOfLegs(number) {
+            if (number) {
+                validateLegs(number);
+                this._numberOfLegs = number;
+            } else {
+                return this._numberOfLegs;
+            }
+        }
+
+        function Animal(type, legs) {
+            species.call(this, type);
+            numberOfLegs.call(this, legs);
+        }
+
+        Animal.prototype = {
+            species: species,
+            legs: numberOfLegs,
+            toSimple:function() {
+                return {
+                    species: this.species(),
+                    legs:this.legs()
+                }
+            }
+        };
+        return Animal;
+    }());
+    return Animal;
+});
