ba74aa9230883cffa051cb9a92c633c892406dff
Geno Genov <g.genov1993@gmail.com>
2014-08-05 13:47:02 +0300

    High quality code homeworks

---
+++ 7.HighQualityCode/15.DesignPatterns/demos/Structural/Flyweight/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Flyweight
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        internal static void Main()
+        {
+            // Build a document with text
+            const string Document = "AAZZBBZB";
+            var chars = Document.ToCharArray();
+
+            var factory = new CharacterFactory();
+
+            // extrinsic state
+            int pointSize = 10;
+
+            // For each character use a flyweight object
+            var list = new List<Character>();
+            foreach (char c in chars)
+            {
+                pointSize++;
+                var character = factory.GetCharacter(c);
+                character.Display(pointSize);
+                list.Add(character);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+            Console.WriteLine();
+            foreach (var c in list)
+            {
+                Console.WriteLine(c.PointSize);
+            }
+        }
+    }
+}
---
+++ 7.HighQualityCode/16.Mocking/dom/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Cars.Tests.JustMock.Mocks
+{
+    using Cars.Contracts;
+    using Cars.Models;
+    using System.Linq;
+    using Telerik.JustMock;
+
+    public class JustMockCarsRepository : CarRepositoryMock, ICarsRepositoryMock
+    {
+        protected override void ArrangeCarsRepositoryMock()
+        {
+            this.CarsData = Mock.Create<ICarsRepository>();
+            Mock.Arrange(() => this.CarsData.Add(Arg.IsAny<Car>())).DoNothing();
+            Mock.Arrange(() => this.CarsData.All()).Returns(this.FakeCarCollection);
+            Mock.Arrange(() => this.CarsData.Search(Arg.AnyString)).Returns(this.FakeCarCollection.Where(c => c.Make == "BMW").ToList());
+            Mock.Arrange(() => this.CarsData.GetById(Arg.AnyInt)).Returns(this.FakeCarCollection.First());
+
+            //Homework Mocks here !
+            Mock.Arrange(() => this.CarsData.GetById(Arg.IsInRange(0,5,RangeKind.Inclusive)))
+                .Returns((int id) => this.FakeCarCollection.FirstOrDefault(c => c.Id == id));
+            Mock.Arrange(() => this.CarsData.SortedByMake()).Returns(this.FakeCarCollection.OrderBy(x=>x.Make).ToList());
+            Mock.Arrange(() => this.CarsData.SortedByYear()).Returns(this.FakeCarCollection.OrderBy(x => x.Year).ToList());
+        }
+    }
+}
