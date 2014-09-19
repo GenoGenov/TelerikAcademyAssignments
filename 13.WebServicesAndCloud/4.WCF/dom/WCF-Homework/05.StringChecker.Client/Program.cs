308a2314ba55d5916c106762871eebaa4933a7a1
Geno Genov <g.genov1993@gmail.com>
2014-09-20 00:49:52 +0300

    WCF Homeworks

---
+++ 13.WebServicesAndCloud/4.WCF/dom/WCF-Homework/05.StringChecker.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using _05.StringChecker.Client.StringCheckerService;
+
+namespace _05.StringChecker.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new StringCheckerSvcClient();
+            int count = client.CheckStrings("aa", "aaaa");
+            Console.WriteLine(count);
+            
+        }
+    }
+}
